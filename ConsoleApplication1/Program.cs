d51e6561b8d4cf30e0c9f6c44ee5796a90591c93
boolean1515 <boolean1515@gmail.com>
2014-03-04 20:29:08 +0600

    12

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                int h;
+                int w;
+                Console.WriteLine("print h");
+                h = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("print w");
+                w = Convert.ToInt32(Console.ReadLine());
+                //объект создать здесь.
+                Rectangle R1 = new Rectangle(h, w);
+                R1.Area();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Rectangle
+    {
+        public int H;
+        public int W;
+
+        public Rectangle(int h, int w)
+        {
+            H = h;
+            W = w;
+        }
+
+        public void Area()
+        {
+            int S = H * W;
+            Console.WriteLine("Площадь" + S);
+        }
+    }
+
+}
